9ce7a96129b494e76cd65e00f7014f45370babfe
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-10-05 00:28:16 +0100

    Add theme switching logic

--- abdullahi.muritala/week 5/src/index.ts
+++ abdullahi.muritala/week 5/src/index.ts
@@ -1,10 +1,27 @@
+import './styles/normalize.css';
 import './styles/index.css';
-const mobileBg = require('./assets/images/bg-mobile-light.jpg');
+import TaskInterface from './task';
+import Task from './task';
 
-const age: number = 99;
-console.log(age);
+export let tasks: TaskInterface[] = [];
+let toggle = document.querySelector('#theme-toggle') as HTMLButtonElement;
 
-const image = new Image();
-image.src = mobileBg;
+// Handle the theme switching logic
+let storedTheme: string =
+  localStorage.getItem('theme') ||
+  (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+if (storedTheme) document.documentElement.setAttribute('data-theme', storedTheme);
 
-document.documentElement.append(image);
+const handleThemeToggle = (): void => {
+  let currentTheme: string | null = document.documentElement.getAttribute('data-theme');
+  let targetTheme = 'light';
+
+  if (currentTheme === 'light') {
+    targetTheme = 'dark';
+  }
+
+  document.documentElement.setAttribute('data-theme', targetTheme);
+  localStorage.setItem('theme', targetTheme);
+};
+
+toggle.addEventListener('click', handleThemeToggle);
