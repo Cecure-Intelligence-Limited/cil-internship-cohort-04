dd9486a31c441e5b09590fca3c8129c9db0bc766
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-10-05 01:13:52 +0100

    implement function to add new tasks

--- abdullahi.muritala/week 5/src/index.ts
+++ abdullahi.muritala/week 5/src/index.ts
@@ -2,9 +2,12 @@ import './styles/normalize.css';
 import './styles/index.css';
 import TaskInterface from './task';
 import Task from './task';
+import render from './render';
 
 export let tasks: TaskInterface[] = [];
 let toggle = document.querySelector('#theme-toggle') as HTMLButtonElement;
+const newTaskForm = document.querySelector('#newTaskForm') as HTMLFormElement;
+export const todos = document.querySelector('#todos') as HTMLUListElement;
 
 // Handle the theme switching logic
 let storedTheme: string =
@@ -24,4 +27,45 @@ const handleThemeToggle = (): void => {
   localStorage.setItem('theme', targetTheme);
 };
 
+const addNewTask = (event: SubmitEvent): void => {
+  event.preventDefault();
+  const titleElement = document.querySelector('#newTodo') as HTMLInputElement;
+  const title = titleElement.value;
+  const checklist = undefined;
+  const newTask = new Task(title, checklist);
+  newTask.addToTasks();
+  saveToLocalStorage();
+  renderTodos();
+  newTaskForm.reset();
+};
+
+// Helper function to clear the ul
+const clearContent = (content: HTMLElement): void => {
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+};
+
+const renderTodos = (): void => {
+  clearContent(todos);
+  tasks.map(render);
+};
+
+const saveToLocalStorage = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const restoreLocalStorage = () => {
+  const value = localStorage.getItem('tasks');
+  if (typeof value === 'string') {
+    tasks = JSON.parse(value);
+  }
+  if (tasks === null) tasks = [];
+  tasks.map(render);
+};
+
 toggle.addEventListener('click', handleThemeToggle);
+newTaskForm.addEventListener('submit', addNewTask);
+
+// Call this on starting the app
+restoreLocalStorage();
