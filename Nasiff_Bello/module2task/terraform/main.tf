9814e99456b74f1afa580be0eade480e7d871f94
Nazzcodek <65385684+Nazzcodek@users.noreply.github.com>
2022-10-06 22:00:46 +0100

    Update main.tf

--- Nasiff_Bello/module2task/terraform/main.tf
+++ Nasiff_Bello/module2task/terraform/main.tf
@@ -1,6 +1,149 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_iam_role" "casual_lambda_role" {
+name   = "myLambdaCasualRole"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+resource "aws_iam_policy" "iam_policy_for_casual_lambda" {
+ 
+ name         = "myLambdaCasualRolePolicy"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws casual lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "attach_casual_iam_policy_to_iam_role" {
+ role        = aws_iam_role.casual_lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_casual_lambda.arn
+}
+
+resource "aws_iam_role" "member_lambda_role" {
+name   = "myLambdaMemberRole"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+resource "aws_iam_policy" "iam_policy_for_member_lambda" {
+ 
+ name         = "myLambdaMemberRolePolicy"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "attach_member_iam_policy_to_iam_role" {
+ role        = aws_iam_role.member_lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_member_lambda.arn
+}
+ 
+data "archive_file" "zip_the_casualpy_code" {
+type        = "zip"
+source_file = "${path.module}/casualLambdaFunction.py" 
+output_path = "casualLambda.zip"
+}
+data "archive_file" "zip_the_memberpy_code" {
+type        = "zip"
+source_file = "${path.module}/memberLambdaFunction.py" 
+output_path = "memberLambda.zip"
+}
+ 
+resource "aws_lambda_function" "terraform_casual_lambda_func" {
+filename                       = "casualLambda.zip"
+function_name                  = "myLambdaCasualRole"
+role                           = aws_iam_role.casual_lambda_role.arn
+handler                        = "casualLambdaFunction.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_casual_iam_policy_to_iam_role]
+}
+ 
+resource "aws_lambda_function" "terraform_member_lambda_func" {
+filename                       = "memberLambda.zip"
+function_name                  = "myLambdaMemberRole"
+role                           = aws_iam_role.member_lambda_role.arn
+handler                        = "memberLambdaFunction.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_member_iam_policy_to_iam_role]
+}
+
+resource "aws_iam_role" "step_role" {
+name   = "myStepRole"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "states.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
 resource "aws_sfn_state_machine" "sfn_state_machine" {
-  name     = "myStateMachine"
-  role_arn = "arn:aws:iam::711328479423:role/StepFunctionRole"
+  name     = "myLambdaStateMachine"
+  role_arn = aws_iam_role.step_role.arn
 
   definition = <<EOF
 {
@@ -24,12 +167,12 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
     },
     "ProcessCasual": {
       "Type": "Task",
-      "Resource": "arn:aws:lambda:us-east-1:711328479423:function:casualLambdaFunction",
+      "Resource": "${aws_lambda_function.terraform_casual_lambda_func.arn}",
       "End": true
     },
     "ProcessMemeber": {
       "Type": "Task",
-      "Resource": "arn:aws:lambda:us-east-1:711328479423:function:memberLambdaFunction",
+      "Resource": "${aws_lambda_function.terraform_member_lambda_func.arn}",
       "End": true
     }
   }
