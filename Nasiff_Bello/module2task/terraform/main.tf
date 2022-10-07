7908c7353345dc6e6e9d8d35d7594bc90c51ea3e
Nazzcodek <65385684+Nazzcodek@users.noreply.github.com>
2022-10-07 12:40:45 +0100

    Update main.tf

--- Nasiff_Bello/module2task/terraform/main.tf
+++ Nasiff_Bello/module2task/terraform/main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+# Casual lambda AWS IAM role, policy and policy attachment 
 resource "aws_iam_role" "casual_lambda_role" {
 name   = "myLambdaCasualRole"
 assume_role_policy = <<EOF
@@ -47,6 +49,7 @@ resource "aws_iam_role_policy_attachment" "attach_casual_iam_policy_to_iam_role"
  policy_arn  = aws_iam_policy.iam_policy_for_casual_lambda.arn
 }
 
+# Member lambda AWS IAM role, policy and policy attachment 
 resource "aws_iam_role" "member_lambda_role" {
 name   = "myLambdaMemberRole"
 assume_role_policy = <<EOF
@@ -92,18 +95,21 @@ resource "aws_iam_role_policy_attachment" "attach_member_iam_policy_to_iam_role"
  role        = aws_iam_role.member_lambda_role.name
  policy_arn  = aws_iam_policy.iam_policy_for_member_lambda.arn
 }
- 
+
+# zipping the python file for the casual and member lambda function
 data "archive_file" "zip_the_casualpy_code" {
 type        = "zip"
 source_file = "${path.module}/casualLambdaFunction.py" 
 output_path = "casualLambda.zip"
 }
+
 data "archive_file" "zip_the_memberpy_code" {
 type        = "zip"
 source_file = "${path.module}/memberLambdaFunction.py" 
 output_path = "memberLambda.zip"
 }
- 
+
+# creating the casual and member lambda function
 resource "aws_lambda_function" "terraform_casual_lambda_func" {
 filename                       = "casualLambda.zip"
 function_name                  = "myLambdaCasualRole"
@@ -122,6 +128,7 @@ runtime                        = "python3.8"
 depends_on                     = [aws_iam_role_policy_attachment.attach_member_iam_policy_to_iam_role]
 }
 
+# step fuction with casual and member lambda fuction attached and step function AWS IAM role
 resource "aws_iam_role" "step_role" {
 name   = "myStepRole"
 assume_role_policy = <<EOF
