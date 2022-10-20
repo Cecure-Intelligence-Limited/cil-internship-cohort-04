8935f28d6b206cc99e423c5c89b6bd6fcf84523f
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-10-20 16:35:44 +0100

    Initial commit

---
+++ abdullahi.muritala/module 3/react-contact-man/src/styles/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  * {
+    box-sizing: border-box;
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: #212529;
+  }
+`;
