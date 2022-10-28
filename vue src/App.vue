803cbf7b8bc53f509a262f13250aa4a147df4829
pipicoker <89666696+pipicoker@users.noreply.github.com>
2022-10-28 16:26:29 +0100

    Add files via upload

---
+++ vue src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <Nav />
+ 
+  <AddTask  />
+  <router-view  @add-task="addTask"></router-view>
+  
+</template>
+
+<script>
+import Nav from './components/Nav.vue';
+import AddTask from './views/Add.vue'
+
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+    
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [task, ...this.tasks]
+    },
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue src/components/Task.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class='flex justify-center'>
+        <div class='flex flex-col justify-center items-center mt-4 border w-[300px] sm:w-[400px] md:w-[800px] '>
+            <div class='flex  justify-between items-center w-5/6   py-8 px-4'>
+                <div class='flex items-center space-x-2'>
+                    <h3>{{task.name}}</h3>
+                    <i class="fas fa-angles-down" @click="toggleDetails"></i>
+
+                </div>
+                <div class='flex items-center space-x-4'>
+                    <i class="fas fa-pen-to-square"></i>
+                    <i class="fas fa-times" @click="$emit('delete-task',task.id)"></i>
+                </div>
+            </div>
+
+            <div v-if="showDetails" >
+                <div class='py-4 px-4 text-left divide-y'>
+                    <p class='pb-4'>Email: <span>{{task.Email}}</span></p>
+                    <p class='pt-4'>Phone: <span>{{task.Phone}}</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    },
+    data() {
+        return {
+            showDetails: false
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.showDetails = !this.showDetails
+        }
+    }
+    
+}
+</script>
