1bff2e3f55122d009fb7198d282cf9796c2512b1
pipicoker <89666696+pipicoker@users.noreply.github.com>
2022-10-28 16:14:08 +0100

    Add files via upload

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='bg-gray-200 m-4 py-12 pl-8 md:mx-32 md:py-16'>
+        <h2 class='text-center font-semibold text-gray-800 text-3xl'>Contact Manager</h2>
+    
+        <p class='mt-2 text-left font-thin text-xl text-gray-700'>This is a portfolio project to showcase knowledge of:
+        </p>
+    
+        <div class='text-left mt-4 text-gray-800 '>
+            <p>i. React Components and JSX.</p>
+            <p>ii. React Router.</p>
+            <p>iii. Component state management.</p>
+            <p>iv. Context api for global state management.</p>
+            <p>v. Making asynchronous calls to the Json placeholder fake rest api using "async/await".</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
