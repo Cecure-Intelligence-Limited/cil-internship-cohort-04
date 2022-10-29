6e68d26d2ae81f2247888c9af869b52748f59e07
devdabiri <mayowa.dabiri@launchbridge.ng>
2022-10-29 02:15:20 +0100

    toggleDetails

--- toluwalase.adeosun/Module3/Vue_Task/vue-contact-manager/src/components/ContactCard.vue
+++ toluwalase.adeosun/Module3/Vue_Task/vue-contact-manager/src/components/ContactCard.vue
@@ -3,7 +3,10 @@
     <div className=" flex items-center p-5 justify-between mt-2">
       <div className=" flex items-center  space-x-4">
         <h1>{{ contact.name }}</h1>
-        <i className="text-[red] fa-solid fa-angles-down"></i>
+        <i
+          className="text-[red] fa-solid fa-angles-down"
+          @click="onShowDetails()"
+        ></i>
         <!-- <BsChevronDoubleDown className="text-[red]" onClick={() =>
         setMoreInfo(!moreInfo)} /> -->
       </div>
@@ -16,7 +19,7 @@
               // editContact(contact.id);
             }}
           /> -->
-        <i class="fas fa-xmark" @click="onDelete(contact.id)"></i>
+        <i class="fas fa-xmark text-[red]" @click="onDelete(contact.id)"></i>
         <!-- <font-awesome-icon icon="fa-solid fa-xmark" /> -->
         <!-- <TiDeleteOutline
             className="text-[red]"
@@ -25,7 +28,7 @@
       </div>
     </div>
 
-    <div className="p-5 ">
+    <div className="p-5 " v-show="showDetails">
       <p className="border divide-[#111011] py-1.5">
         <strong>Email: </strong>
         {{ contact.email }}
@@ -44,8 +47,16 @@ export default {
   props: {
     contact: Object,
   },
+  data() {
+    return {
+      showDetails: false,
+    };
+  },
 
   methods: {
+    onShowDetails() {
+      this.showDetails = !this.showDetails;
+    },
     onDelete(id) {
       this.$emit("delete-contact", id);
     },
